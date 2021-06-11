fe3070a45517ccae234e22b2d0b377a9cf6f75e6
Suzie K <suziekim.dev@gmail.com>
2021-06-10 22:45:41 -0400

    Green test links to bulk discount index page

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -1,6 +1,7 @@
 class Merchants::BulkDiscountsController < ApplicationController
 
   def index
+    @merchant = Merchant.find(params[:merchant_id])
   end
 
   def show
---
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -0,0 +1,12 @@
+# spec/features/merchants/bulk_discounts/index_spec
+
+require 'rails_helper'
+
+RSpec.describe 'Merchants dashboard show page' do
+  describe 'display' do
+    it 'lists all the bulk discounts and attributes' 
+
+    it 'links to each individual bulk discount show page'
+  end
+
+end
