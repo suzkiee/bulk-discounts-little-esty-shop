ad7ac3fefb6f334845fcf349758c5ed22b46e520
Suzie K <suziekim.dev@gmail.com>
2021-06-13 16:27:53 -0400

    Green test links to applied discount show page

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -9,7 +9,6 @@ class Merchants::InvoicesController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = Invoice.find(params[:id])
     @invoice_items = @invoice.invoice_items
-    
   end
 
   def update
