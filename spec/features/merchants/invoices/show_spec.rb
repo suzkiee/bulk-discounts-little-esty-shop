a348f81ab30c45b887e6454f11902edb6ddb38a9
Suzie K <suziekim.dev@gmail.com>
2021-06-13 16:03:31 -0400

    Red test lists total discounted revenue for merchant

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -4,26 +4,38 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant invoice show page' do
   before :each do
-    # @item = Item.find(id)
-    # @merchant = Merchant.find(id)
-    @merchant = Merchant.create!(name: 'Sally Handmade')
-    @merchant_2 = Merchant.create!(name: 'Billy Mandmade')
-    @item =  @merchant.items.create!(name: 'Qui Essie', description: 'Lorem ipsim', unit_price: 1200)
-    @item_2 =  @merchant.items.create!(name: 'Essie', description: 'Lorem ipsim', unit_price: 1000)
-    @item_3 = @merchant_2.items.create!(name: 'Glowfish Markdown', description: 'Lorem ipsim', unit_price: 200)
-    @customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka') 
-    @invoice = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    @invoice_2 = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    InvoiceItem.create!(item_id: @item.id, invoice_id: @invoice.id, quantity: 3, unit_price: 1200, status: 1)
-    InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice.id, quantity: 10, unit_price: 1000, status: 1)
-    InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 12, unit_price: 200, status: 1)
+    # @merchant = Merchant.create!(name: 'Sally Handmade')
+    # @merchant_2 = Merchant.create!(name: 'Billy Mandmade')
+    # @item =  @merchant.items.create!(name: 'Qui Essie', description: 'Lorem ipsim', unit_price: 1200)
+    # @item_2 =  @merchant.items.create!(name: 'Essie', description: 'Lorem ipsim', unit_price: 1000)
+    # @item_3 = @merchant_2.items.create!(name: 'Glowfish Markdown', description: 'Lorem ipsim', unit_price: 200)
+    # @customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka') 
+    # @invoice = Invoice.create!(customer_id: @customer.id, status: 'completed')
+    # @invoice_2 = Invoice.create!(customer_id: @customer.id, status: 'completed')
+    # InvoiceItem.create!(item_id: @item.id, invoice_id: @invoice.id, quantity: 5, unit_price: 1200, status: 1)
+    # InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice.id, quantity: 10, unit_price: 1000, status: 1)
+    # InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 12, unit_price: 200, status: 1)
+    # visit merchant_invoice_path(@merchant, @invoice)
+    # @discount = create(:mock_bulk_discount, threshold: 5, percent_discount: 50,merchant: @merchant)
+    @merchant = create(:mock_merchant)
+    @merchant_2 = create(:mock_merchant)
+    @item = create(:mock_item, merchant: @merchant)
+    @item_2 = create(:mock_item, merchant: @merchant)
+    @item_3 = create(:mock_item, merchant: @merchant_2) 
+    @customer = create(:mock_customer, first_name: 'Joey', last_name: 'Ondricka')
+    @invoice = create(:mock_invoice, customer: @customer)
+    @invoice_2 = create(:mock_invoice, customer: @customer)
+    @invoice_item = create(:mock_invoice_item, item: @item, invoice: @invoice, quantity: 5, unit_price: 1200) 
+    invoice_item_2 = create(:mock_invoice_item, item: @item_2, invoice: @invoice, quantity: 10, unit_price: 1000) 
+    invoice_item_3 = create(:mock_invoice_item, item: @item_3, invoice: @invoice_2, quantity: 12, unit_price: 200)
+    discount = create(:mock_bulk_discount, threshold: 10, percent_discount: 50, merchant: @merchant)
+    visit merchant_invoice_path(@merchant, @invoice)
   end
  
   describe 'display' do
     it 'shows invoice and its attributes' do
       created_at = @invoice.created_at.strftime('%A, %B %d, %Y')
-      visit "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
- 
+
       expect(page).to have_content("INVOICE # #{@invoice.id}")
       expect(page).to_not have_content("INVOICE # #{@invoice_2.id}")
       expect(page).to have_content("#{@invoice.status}")
@@ -31,17 +43,15 @@ RSpec.describe 'Merchant invoice show page' do
     end
 
     it 'lists all items and item attributes on the invoice' do
-      visit "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
-
-      expect(page).to have_content("Qui Essie")
-      expect(page).to have_content("Essie")
-      expect(page).to_not have_content("Glowfish Markdown")
-      expect(page).to have_content("3")
+      
+      expect(page).to have_content("#{@item.name}")
+      expect(page).to have_content("#{@item_2.name}")
+      expect(page).to_not have_content("#{@item_3.name}")
+      expect(page).to have_content("#{@invoice_item.quantity}")
       expect(page).to have_content("$1,200.00")
     end
 
     it 'can update items status through dropdown list' do
-      visit "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
 
       expect(page).to have_button("Save")
       
@@ -57,9 +67,22 @@ RSpec.describe 'Merchant invoice show page' do
     end
 
     it 'lists total revenue of all items on invoice' do 
-      visit "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
       
-      expect(page).to have_content("Expected Total Revenue: $136.00")
+      expect(page).to have_content("Expected Total Revenue: $160.00")
     end
+
+    it 'lists total discounted revenue for merchant' do
+
+      save_and_open_page
+      expect(page).to have_content("Expected Total Discounted Revenue: $115.00")
+    end
+
+    # it 'links to applied discount show page' do
+
+      
+    #   click_on(discount.name)
+
+    #   expect(current_path).to eq(merchant_bulk_discount_path(@merchant, discount))
+    # end
   end
 end
