13fccf54a53e5d11d9a6051478b41cee4f730136
Suzie K <suziekim.dev@gmail.com>
2021-06-13 16:27:41 -0400

    Red test links to applied discount show page

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -4,19 +4,6 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant invoice show page' do
   before :each do
-    # @merchant = Merchant.create!(name: 'Sally Handmade')
-    # @merchant_2 = Merchant.create!(name: 'Billy Mandmade')
-    # @item =  @merchant.items.create!(name: 'Qui Essie', description: 'Lorem ipsim', unit_price: 1200)
-    # @item_2 =  @merchant.items.create!(name: 'Essie', description: 'Lorem ipsim', unit_price: 1000)
-    # @item_3 = @merchant_2.items.create!(name: 'Glowfish Markdown', description: 'Lorem ipsim', unit_price: 200)
-    # @customer = Customer.create!(first_name: 'Joey', last_name: 'Ondricka') 
-    # @invoice = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    # @invoice_2 = Invoice.create!(customer_id: @customer.id, status: 'completed')
-    # InvoiceItem.create!(item_id: @item.id, invoice_id: @invoice.id, quantity: 5, unit_price: 1200, status: 1)
-    # InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice.id, quantity: 10, unit_price: 1000, status: 1)
-    # InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_2.id, quantity: 12, unit_price: 200, status: 1)
-    # visit merchant_invoice_path(@merchant, @invoice)
-    # @discount = create(:mock_bulk_discount, threshold: 5, percent_discount: 50,merchant: @merchant)
     @merchant = create(:mock_merchant)
     @merchant_2 = create(:mock_merchant)
     @item = create(:mock_item, merchant: @merchant)
@@ -28,7 +15,7 @@ RSpec.describe 'Merchant invoice show page' do
     @invoice_item = create(:mock_invoice_item, item: @item, invoice: @invoice, quantity: 5, unit_price: 1200) 
     invoice_item_2 = create(:mock_invoice_item, item: @item_2, invoice: @invoice, quantity: 10, unit_price: 1000) 
     invoice_item_3 = create(:mock_invoice_item, item: @item_3, invoice: @invoice_2, quantity: 12, unit_price: 200)
-    discount = create(:mock_bulk_discount, threshold: 10, percent_discount: 50, merchant: @merchant)
+    @discount = create(:mock_bulk_discount, threshold: 10, percent_discount: 50, merchant: @merchant)
     visit merchant_invoice_path(@merchant, @invoice)
   end
  
@@ -72,17 +59,15 @@ RSpec.describe 'Merchant invoice show page' do
     end
 
     it 'lists total discounted revenue for merchant' do
-
-      save_and_open_page
-      expect(page).to have_content("Expected Total Discounted Revenue: $115.00")
+      total_discounted_revenue = @invoice.revenue.to_f/100 - @invoice.total_discount.to_f/100
+      expect(page).to have_content("Expected Total Discounted Revenue: $#{total_discounted_revenue}")
     end
 
-    # it 'links to applied discount show page' do
+    it 'links to applied discount show page' do
 
-      
-    #   click_on(discount.name)
+      click_on(@discount.name)
 
-    #   expect(current_path).to eq(merchant_bulk_discount_path(@merchant, discount))
-    # end
+      expect(current_path).to eq(merchant_bulk_discount_path(@merchant, @discount))
+    end
   end
 end
