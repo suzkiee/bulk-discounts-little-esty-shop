56ceeb0f3cf34bc44e8c6db75cb9c3d8cdf69c04
Suzie K <suziekim.dev@gmail.com>
2021-06-14 11:50:50 -0400

    Red test lists total discounted revenue from this invoice

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe 'invoices show page', type: :feature do
 
   describe 'page appearance' do
     it 'has all info about that invoice' do
-      visit "/admin/invoices/#{@invoice.id}"
+      visit admin_invoice_path(@invoice) 
       date = @invoice.created_at.strftime("%A, %B %d, %Y")
       expect(page).to have_content @invoice.status.capitalize
       expect(page).to have_content @invoice.id
@@ -19,9 +19,9 @@ RSpec.describe 'invoices show page', type: :feature do
     end
 
     it 'has information about all of the invoice\'s items' do
-      visit "/admin/invoices/#{@invoice.id}"
-      first_exp_price = "$#{@first_invoice_item.unit_price / 100.0}"
-      last_exp_price = "$#{@last_invoice_item.unit_price / 100.0}"
+      visit admin_invoice_path(@invoice)
+      first_exp_price = "#{@first_invoice_item.unit_price / 100.0}"
+      last_exp_price = "#{@last_invoice_item.unit_price / 100.0}"
       expect(page).to have_content @first_invoice_item.item.name
       expect(page).to have_content @first_invoice_item.quantity
       expect(page).to have_content first_exp_price
@@ -33,17 +33,29 @@ RSpec.describe 'invoices show page', type: :feature do
     end
 
     it 'lists total revenue from this invoice' do
-      visit "/admin/invoices/#{@invoice.id}"
-      exp_revenue = "$#{@invoice.revenue / 100.0}"
+      visit admin_invoice_path(@invoice)
+      exp_revenue = "$21,067.77"
       within "section#revenue" do
         expect(page).to have_content exp_revenue
       end
+    end
+
+    it 'lists total discounted revenue from this invoice' do
+      merchant = create(:mock_merchant)
+      item = create(:mock_item, merchant: merchant)
+      customer = create(:mock_customer)
+      invoice = create(:mock_invoice, customer: customer)
+      invoice_item = create(:mock_invoice_item, item: item, invoice: invoice, quantity: 10, unit_price: 100)
+      discount = create(:mock_bulk_discount, threshold: 5, percent_discount: 50, merchant: merchant)
+      visit admin_invoice_path(invoice)
+
+      expect(page).to have_content("Expected Total Discounted Revenue: $5.00")
     end
   end
 
   describe 'page functionality' do
     it 'can update invoice status' do
-      visit "/admin/invoices/#{@invoice.id}"
+      visit admin_invoice_path(@invoice)
       expected_status = @invoice.status.capitalize
       expect(page).to have_select('invoice_status', selected: expected_status)
       select 'Completed', from: 'invoice_status' 
